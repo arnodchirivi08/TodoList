16538cf211872e0a4b52e74e1b5323705c7755c4
arnodchirivi08 <arnod.chirivi@gmail.com>
2026-01-18 18:16:11 -0500

    🟥 Debe_EstarVacia_CuandoSeCreaUnaListaDeTareas

--- TodoTdd.Tests/TodoListTests.cs
+++ TodoTdd.Tests/TodoListTests.cs
@@ -14,21 +14,6 @@ namespace TodoTdd.Tests
 
             tareas.Should().BeEmpty();
         }
-        //[Fact]
-        //public void Debe_TenerUnaTarea_CuandoSeAgregaUnaTareaAListaVacia()
-        //{
-        //    // Arrange (Dado)
-        //    var todo = new Todo();
-        //    var tareaAAgregar = "Tarea 1";
-
-        //    // Act (Cuando)
-        //    todo.AgregarTarea(tareaAAgregar);
-        //    List<string> tareas = todo.ObtenerTareas();
-
-        //    // Assert (Entonces)
-        //    tareas.Should().Contain(tareaAAgregar);
-        //    tareas.Should().HaveCount(1);
-        //}
     }
 
     public class Todo
