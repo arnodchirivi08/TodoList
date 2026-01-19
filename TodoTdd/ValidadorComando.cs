8a485ea88c0631bb25df508d38d940a2102fd543
arnodchirivi08 <arnod.chirivi@gmail.com>
2026-01-18 21:06:29 -0500

    🟩 Debe_SerComandoValido_CuandoEs_a_minuscula

--- TodoTdd/ValidadorComando.cs
+++ TodoTdd/ValidadorComando.cs
@@ -5,7 +5,7 @@ namespace TodoTdd
     {
         public bool EsValido(string opcion)
         {
-            return opcion == "S" || opcion == "s" || opcion == "A";
+            return opcion == "S" || opcion == "s" || opcion == "A" || opcion == "a";
         }
     }
 }
