88541aee96f6cb5f89cb1f8d5ff87468565dadfd
arnodchirivi08 <arnod.chirivi@gmail.com>
2026-01-18 19:46:37 -0500

    🟦 Debe_GenerarUnaAlertaDeError_CuandoLaDescripcionDeLatareaYaExiste

--- TodoTdd/ListaDeTareas.cs
+++ TodoTdd/ListaDeTareas.cs
@@ -11,12 +11,16 @@
 
         public void AgregarTarea(string tarea)
         {
+            ValidarDescripcionTarea(tarea);
+            tareas.Add(tarea);
+        }
+
+        private void ValidarDescripcionTarea(string tarea)
+        {
             if (String.IsNullOrEmpty(tarea))
                 throw new ArgumentException("La descripción no puede estar vacia");
             if (tareas.Contains(tarea))
                 throw new ArgumentException("La descripción no puede ser duplicada");
-
-            tareas.Add(tarea);
         }
     }
 }
